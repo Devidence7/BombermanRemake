d2c786e38cf977736629c45c386b7cab022f0198
Alonso Muñoz <33006411+Devidence7@users.noreply.github.com>
2020-02-18 16:30:35 +0100

    main+Cmake

---
+++ cmake-build-debug/CMakeFiles/Videogames.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Videogames.dir/main.cpp.obj"
+  "Videogames.exe"
+  "Videogames.exe.manifest"
+  "Videogames.pdb"
+  "libVideogames.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Videogames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Videogames.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Videogames.
+# This may be replaced when dependencies are built.
