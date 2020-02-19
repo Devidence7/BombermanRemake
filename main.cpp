e21e77aff12fe39ff3557a56f29fb75c52daff3f
rankern <victorm.lafuente@gmail.com>
2020-02-19 09:22:29 +0100

    base

--- main.cpp
+++ main.cpp
@@ -1,118 +1,74 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <GL/glut.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
 
+int tamImageX = 1200, tamImgaeY = 600;
+float DireccionX = 0.4, DireccionY = 1;
 
-GLenum doubleBuffer;
-GLint thing1, thing2;
+int tamSpriteX = 0, tamSpriteY = 0;
 
-
-static void Init(void)
+int actualizarShape(float &x, float &y)
 {
+    x += DireccionX;
+    y += DireccionY;
+    if (x < 0 || x + tamSpriteX*2 > tamImageX)
+    {
+        //rebotar
+        DireccionX = -DireccionX;
+    }
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glClearAccum(0.0, 0.0, 0.0, 0.0);
-
-    thing1 = glGenLists(1);
-    glNewList(thing1, GL_COMPILE);
-    glColor3f(1.0, 0.0, 0.0);
-    glRectf(-1.0, -1.0, 1.0, 0.0);
-    glEndList();
-
-    thing2 = glGenLists(1);
-    glNewList(thing2, GL_COMPILE);
-    glColor3f(0.0, 1.0, 0.0);
-    glRectf(0.0, -1.0, 1.0, 1.0);
-    glEndList();
-}
-
-static void Reshape(int width, int height)
-{
-
-    glViewport(0, 0, width, height);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-}
-
-static void Key(unsigned char key, int x, int y)
-{
-
-    switch (key) {
-      case '1':
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glutPostRedisplay();
-	break;
-      case '2':
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glutPostRedisplay();
-	break;
-      case 27:
-	exit(0);
+    if (y < 0 || y + tamSpriteY*2 > tamImgaeY)
+    {
+        std::cout << y << std::endl;
+        DireccionY = -DireccionY;
     }
 }
 
-static void Draw(void)
+int main()
 {
-
-    glPushMatrix();
-
-    glScalef(0.8, 0.8, 1.0);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glCallList(thing1);
-    glAccum(GL_LOAD, 0.5);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glCallList(thing2);
-    glAccum(GL_ACCUM, 0.5);
-
-    glAccum(GL_RETURN, 1.0);
-
-    glPopMatrix();
-
-    if (doubleBuffer) {
-	glutSwapBuffers();
-    } else {
-	glFlush();
+    sf::RenderWindow window(sf::VideoMode(tamImageX, tamImgaeY), "Rebotes!");
+    sf::CircleShape shape(100.f);
+    //sf::RectangleShape shape(100.f)
+    shape.setFillColor(sf::Color::Green);
+    //textura
+    sf::Texture texture;
+    if (!texture.loadFromFile("image.jpeg"))
+    {
     }
-}
+    tamSpriteX = texture.getSize().x / 2;
+    tamSpriteY = texture.getSize().y / 2;
 
-static void Args(int argc, char **argv)
-{
-    GLint i;
+    float posX = 0, posY = 0;
 
-    doubleBuffer = GL_FALSE;
+    //if (!texture.loadFromFile("image.jpeg", sf::IntRect(32, 32, 32, 32)))
+    //{
+    //    // error...
+    //}
+    sf::Sprite sprite;
+    sprite.setPosition(sf::Vector2f(tamImageX / 2, tamImgaeY / 2));
+    sprite.setTexture(texture);
+    window.clear();
+    //window.draw(shape);
+    window.draw(sprite);
+    window.display();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    for (i = 1; i < argc; i++) {
-	if (strcmp(argv[i], "-sb") == 0) {
-	    doubleBuffer = GL_FALSE;
-	} else if (strcmp(argv[i], "-db") == 0) {
-	    doubleBuffer = GL_TRUE;
-	}
+        window.clear();
+        //window.draw(shape);
+        window.draw(sprite);
+        window.display();
+        sleep(0.3);
+        actualizarShape(posX, posY);
+        sprite.setPosition(sf::Vector2f(posX, posY));
     }
-}
 
-int main(int argc, char **argv)
-{
-    GLenum type;
-
-    glutInit(&argc, argv);
-    Args(argc, argv);
-
-    type = GLUT_RGB | GLUT_ACCUM;
-    type |= (doubleBuffer) ? GLUT_DOUBLE : GLUT_SINGLE;
-    glutInitDisplayMode(type);
-    glutInitWindowSize(300, 300);
-    glutCreateWindow("Accum Test");
-
-    Init();
-
-    glutReshapeFunc(Reshape);
-    glutKeyboardFunc(Key);
-    glutDisplayFunc(Draw);
-    glutMainLoop();
+    return 0;
 }
